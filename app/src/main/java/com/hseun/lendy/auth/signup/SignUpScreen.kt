8db8926d3f2882043a821d254512ac57913bddcf
hseun <hseun0732@dms.hs.kr>
2025-04-18 09:04:43 +0900

    ✨ :: SignUp

---
+++ app/src/main/java/com/hseun/lendy/auth/signup/SignUpScreen.kt
@@ -0,0 +1,139 @@
+package com.hseun.lendy.auth.signup
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.hseun.lendy.R
+import com.hseun.lendy.auth.signup.viewmodel.SignUpViewModel
+import com.hseun.lendy.ui.AuthButton
+import com.hseun.lendy.ui.LendyInput
+import com.hseun.lendy.ui.LendyPasswordInput
+import com.hseun.lendy.ui.theme.White
+import com.hseun.lendy.ui.utils.addFocusCleaner
+import com.hseun.lendy.utils.InputErrorType
+
+const val NAVIGATION_SIGNUP = "signUp"
+
+@Composable
+fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    viewModel: SignUpViewModel = hiltViewModel(),
+    onSuccess: (String) -> Unit,
+    navToSignIn: () -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+
+    val userId = viewModel.userId
+    val password = viewModel.password
+    val checkPassword = viewModel.checkPassword
+
+    val idErrorType = viewModel.idErrorType
+    val pwErrorType = viewModel.pwErrorType
+    val checkPwErrorType = viewModel.checkPwErrorType
+
+    val isLoading = viewModel.isLoading
+    val isSignUpSuccess = viewModel.isSignUpSuccess
+    val buttonEnabled by remember(isLoading, idErrorType, pwErrorType, checkPwErrorType) {
+        derivedStateOf {
+            !isLoading &&
+            userId.isNotEmpty() &&
+            password.isNotEmpty() &&
+            idErrorType == InputErrorType.NONE &&
+            pwErrorType == InputErrorType.NONE &&
+            checkPwErrorType == InputErrorType.NONE
+        }
+    }
+
+    if (isSignUpSuccess) {
+        onSuccess(userId)
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(White)
+            .addFocusCleaner(focusManager)
+    ) {
+        Column(
+            modifier = modifier.align(Alignment.TopCenter),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Spacer(modifier = modifier.height(80.dp))
+            Image(
+                modifier = modifier
+                    .padding(
+                        start = 120.dp,
+                        end = 120.dp
+                    )
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                painter = painterResource(id = R.drawable.lendy_logo_auth),
+                contentDescription = "Lendy Logo",
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = modifier.height(52.dp))
+            LendyInput(
+                label = stringResource(id = R.string.auth_id),
+                input = userId,
+                hint = stringResource(id = R.string.auth_id),
+                imeAction = ImeAction.Next,
+                errorType = idErrorType,
+                onValueChange = { input ->
+                    viewModel.onIdChanged(input)
+                }
+            )
+            LendyPasswordInput(
+                label = stringResource(id = R.string.auth_pw),
+                input = password,
+                hint = stringResource(id = R.string.auth_pw),
+                imeAction = ImeAction.Next,
+                errorType = pwErrorType,
+                onValueChange = { input ->
+                    viewModel.onPwChanged(input)
+                }
+            )
+            LendyPasswordInput(
+                label = stringResource(id = R.string.auth_pw_check),
+                input = checkPassword,
+                hint = stringResource(id = R.string.auth_pw_check),
+                imeAction = ImeAction.Done,
+                errorType = checkPwErrorType,
+                onValueChange = { input ->
+                    viewModel.onCheckPwChanged(input)
+                }
+            )
+        }
+        AuthButton(
+            modifier = modifier.align(Alignment.BottomCenter),
+            enabled = buttonEnabled,
+            buttonText = stringResource(id = R.string.signup),
+            isNotText = stringResource(id = R.string.signup_is_member),
+            moveToWhereText = stringResource(id = R.string.signup_go_login),
+            onButtonClick = {
+                viewModel.onSignUpClick()
+            },
+            onTextClick = navToSignIn
+        )
+    }
+}
---
+++ app/src/main/java/com/hseun/lendy/auth/signup/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,66 @@
+package com.hseun.lendy.auth.signup.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hseun.lendy.auth.AuthRepository
+import com.hseun.lendy.utils.InputErrorType
+import com.hseun.lendy.utils.InputRegexType
+import com.hseun.lendy.utils.checkInputRegex
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val repository: AuthRepository
+) : ViewModel() {
+    var userId by mutableStateOf("")
+    var password by mutableStateOf("")
+    var checkPassword by mutableStateOf("")
+
+    var isLoading by mutableStateOf(false)
+    var isSignUpSuccess by mutableStateOf(false)
+
+    var idErrorType by mutableStateOf(InputErrorType.NONE)
+    var pwErrorType by mutableStateOf(InputErrorType.NONE)
+    var checkPwErrorType by mutableStateOf(InputErrorType.NONE)
+
+    fun onIdChanged(input: String) {
+        userId = input
+        idErrorType = if (checkInputRegex(InputRegexType.ID, input)) InputErrorType.NONE else InputErrorType.ID_REGEX
+    }
+
+    fun onPwChanged(input: String) {
+        password = input
+        pwErrorType = if (checkInputRegex(InputRegexType.PASSWORD, input)) InputErrorType.NONE else InputErrorType.PW_REGEX
+        checkPwErrorType = if (password == checkPassword) InputErrorType.NONE else InputErrorType.NOT_SAME_PW
+    }
+
+    fun onCheckPwChanged(input: String) {
+        checkPassword = input
+        checkPwErrorType = if (password == checkPassword) InputErrorType.NONE else InputErrorType.NOT_SAME_PW
+    }
+
+    fun onSignUpClick() {
+        viewModelScope.launch {
+            isLoading = true
+            val result = repository.signUp(userId, password)
+            isLoading = false
+            result.onSuccess {
+                isSignUpSuccess = true
+                Log.d("signUp", result.toString())
+            }.onFailure {
+                Log.d("signUp", it.message.toString())
+                if (it.message == "Overlap userId") {
+                    idErrorType = InputErrorType.OVERLAP_ID
+                } else {
+                    isSignUpSuccess = false
+                }
+            }
+        }
+    }
+}
