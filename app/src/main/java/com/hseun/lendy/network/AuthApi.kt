76da2e2f1fa55cc356f8ac9854b0a7fa33113911
hseun <hseun0732@dms.hs.kr>
2025-04-18 09:03:41 +0900

    🌱 :: AuthApi

---
+++ app/src/main/java/com/hseun/lendy/network/AuthApi.kt
@@ -0,0 +1,24 @@
+package com.hseun.lendy.network
+
+import com.hseun.lendy.auth.signin.model.SignInRequest
+import com.hseun.lendy.auth.signin.model.SignInResponse
+import com.hseun.lendy.auth.signup.data.SignUpRequest
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface AuthApi {
+    @POST("/auth/signup")
+    suspend fun signUp(@Body request: SignUpRequest): Response<Unit>
+
+    @POST("/auth/signin")
+    suspend fun signIn(@Body request: SignInRequest): Response<SignInResponse>
+
+    @POST("/auth/refresh")
+    suspend fun refresh(@Header("authorization") token: String): Response<SignInResponse>
+}
