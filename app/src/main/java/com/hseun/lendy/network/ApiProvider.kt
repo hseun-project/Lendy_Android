f20f5a2cfae13cb7cfb489ebd922ed596eabe1c2
hseun <hseun0732@dms.hs.kr>
2025-05-02 09:42:02 +0900

    delete :: import

--- app/src/main/java/com/hseun/lendy/network/ApiProvider.kt
+++ app/src/main/java/com/hseun/lendy/network/ApiProvider.kt
@@ -6,7 +6,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.annotation.Signed
 import javax.inject.Singleton
 
 @Module
